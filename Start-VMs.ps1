3afe52f979b3454759508dc8fb57cf86d1926a3b
Wes Taylor <150176347+WTaylor8miami@users.noreply.github.com>
2024-10-02 20:02:33 -0400

    Create Start-VMs.ps1

---
+++ Start-VMs.ps1
@@ -0,0 +1,25 @@
+param (
+    [string]$vCenterServer,
+    [string]$vCenterUser,
+    [string]$vCenterPass,
+    [string[]]$VMNameList
+)
+
+# Import VMware PowerCLI
+Import-Module VMware.PowerCLI
+
+# Connect to the vCenter Server
+Connect-VIServer -Server $vCenterServer -User $vCenterUser -Password $vCenterPass
+
+foreach ($vmName in $VMNameList) {
+    $vm = Get-VM -Name $vmName
+    if ($vm) {
+        Start-VM -VM $vm -Confirm:$false
+        Write-Output "VM '$vmName' started successfully."
+    } else {
+        Write-Output "VM '$vmName' not found."
+    }
+}
+
+# Disconnect from vCenter Server
+Disconnect-VIServer -Server $vCenterServer -Confirm:$false
