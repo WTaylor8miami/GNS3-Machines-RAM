dc5c2aae740a37aaa552449108bf88abeb811982
Wes Taylor <150176347+WTaylor8miami@users.noreply.github.com>
2024-10-02 20:49:50 -0400

    Update Change-RAM.ps1

--- Change-RAM.ps1
+++ Change-RAM.ps1
@@ -8,29 +8,36 @@ param(
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -Scope User
 
 # Connect to the vCenter Server using the provided credentials
+Write-Host "Connecting to vCenter Server: $vCenterServer"
 Connect-VIServer -Server $vCenterServer -User $vCenterUser -Password $vCenterPass
 
-# List of VMs to update RAM
+# List of VMs to change RAM
 $vmList = @(
     "386-00",
     "386-01"
 )
 
-# Loop through each VM name and update RAM if found
+# Loop through each VM name and change RAM if found
 foreach ($vmName in $vmList) {
-    try {
-        $vm = Get-VM -Name $vmName
-        if ($vm -ne $null) {
-            Write-Host "Changing RAM for VM: $vmName to 96GB"
-            Set-VM -VM $vm -MemoryGB 96 -Confirm:$false -Verbose
-            Write-Host "RAM for VM '$vmName' changed to 96GB successfully."
-        } else {
-            Write-Host "VM not found: $vmName"
+    Write-Host "Processing VM name: '$vmName'"
+    if (-not [string]::IsNullOrWhiteSpace($vmName)) {
+        try {
+            Write-Host "Attempting to get VM '$vmName'..."
+            $vm = Get-VM -Name $vmName -ErrorAction Stop
+
+            if ($vm) {
+                Write-Host "Changing RAM for VM: $vmName to 96GB..."
+                Set-VM -VM $vm -MemoryGB 96 -Confirm:$false -Verbose
+                Write-Host "RAM for VM '$vmName' changed to 96GB successfully."
+            }
+        } catch {
+            Write-Host "Error processing VM '$vmName': $_"
         }
-    } catch {
-        Write-Host "Error processing VM: $vmName. Error: $_"
+    } else {
+        Write-Host "Skipped an empty or invalid VM name."
     }
 }
 
 # Disconnect from the vCenter Server after operations
+Write-Host "Disconnecting from the vCenter Server..."
 Disconnect-VIServer -Server $vCenterServer -Confirm:$false
