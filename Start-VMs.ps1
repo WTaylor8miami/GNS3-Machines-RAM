aeb3e9de5e55a9f1841fbccef1f26d6e85c22213
Wes Taylor <150176347+WTaylor8miami@users.noreply.github.com>
2024-10-02 20:33:19 -0400

    Update Start-VMs.ps1

--- Start-VMs.ps1
+++ Start-VMs.ps1
@@ -1,12 +1,19 @@
+# Define the list of VMs to be started
+$vmList = @(
+    "284-01", "284-02", "284-03", "284-04", "284-05", "284-06", "284-07", "284-08", "284-09", "284-10",
+    "284-11", "284-12", "284-13", "284-14", "284-15", "284-16", "284-17", "386-00", "386-01",
+    "358-01", "358-02", "358-03", "358-04", "358-05", "358-06", "358-07", "358-08", "358-09", "358-10",
+    "358-11", "358-12", "358-13", "358-14",
+    "281K-01", "281K-02", "281K-03", "281K-04", "281K-05",
+    "281-01", "281-02", "281-03", "281-04", "281-05", "281-06", "281-07", "281-08", "281-09", "281-10", 
+    "281-11", "281-12", "281-13", "281-14"
+)
+
 param (
     [string]$vCenterServer,
     [string]$vCenterUser,
-    [string]$vCenterPass,
-    [string]$VMNameList
+    [string]$vCenterPass
 )
-
-# Convert the comma-separated VM names to an array
-$VMNamesArray = $VMNameList -split ','
 
 # Import VMware PowerCLI
 Import-Module VMware.PowerCLI
@@ -17,7 +24,7 @@ Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 # Connect to the vCenter Server
 Connect-VIServer -Server $vCenterServer -User $vCenterUser -Password $vCenterPass
 
-foreach ($vmName in $VMNamesArray) {
+foreach ($vmName in $vmList) {
     $vm = Get-VM -Name $vmName.Trim()
     if ($vm) {
         Start-VM -VM $vm -Confirm:$false
